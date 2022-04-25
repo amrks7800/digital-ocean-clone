4becec0bae4ebc371f74698c36a40e1242632ff2
amro khaled EG <amrks780@gmail.com>
2022-04-26 00:42:31 +0200

    initial commit

---
+++ JS/main.js
@@ -0,0 +1,8 @@
+// navigation bar responsive design
+let navIcon = document.querySelector("header .fa-pause");
+let nav = document.querySelector("header nav");
+navIcon.onclick = function () {
+  nav.classList.toggle("open");
+};
+
+// customizing savings card.
